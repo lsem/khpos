f29d426316b36b90b1bac3bd4c7bd976c1312379
lyubomyr.semkiv <lyubomyr.semkiv@hotmail.com>
2018-07-29 22:52:39 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require('express')
+
+STATE_CONNECTED = 'connected';
+STATE_NOTCONNECTED = 'not-connected';
+RECONNECT_TIMEOUT = 5000
+
+//////////////////////////////////////////////////////////////////
+
+class PosterProxyService {
+  constructor() {
+    this.onDisconnectedToPosterCb = null
+    this.onConnectedToPosterCb = null
+    this.connectionState = STATE_NOTCONNECTED
+    this.reconnectRetries = 0
+  }
+
+  onConnectedToPoster(cb) {
+    this.onConnectedToPosterCb = cb
+  }
+
+  onDisconnectedToPoster(cb) {
+    this.onDisconnectedToPosterCb = cb
+  }
+
+  initiateConnect() {
+  }
+
+  scheduleConnectIfNeeded() {
+    console.log('posproxy: reconnect')
+    setTimeout(() => {
+      this.initiateConnect()
+      this.reconnectRetries += 1
+      if (this.reconnectRetries === 5) {
+        this.onConnectedToPosterCb()
+        this.reconnectRetries = 0
+      } else {
+        this.scheduleConnectIfNeeded()
+      }
+    }, 1000)
+  }
+
+  tryConnectToPoster() {
+    console.log('trying connect to poster ..')
+    setTimeout(()=>{
+      this.tryConnectToPoster()
+    }, RECONNECT_TIMEOUT)
+  }
+
+  start() {
+    this.scheduleConnectIfNeeded()
+  }
+}
+
+ //////////////////////////////////////////////////////////////////
+
+class KultHlibaPointOfSaleService {
+  constructor() {
+    var expressHttp = express()
+  }
+
+  connectedToPoster() {
+    console.log('kh: Connected to Poster')
+  }
+
+  disconnectedFromPoster() {
+    console.log('kh: Disconnected from Poster')
+  }
+
+  start() {
+  }
+
+}
+
+/////////////////////////////////////////////////////////
+
+class KultHlibaWebApp {
+  constructor(){
+    this.app = express()
+    this.app.get('/', this.helloWorld )
+  }
+
+  start() {
+    this.app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  }
+
+  helloWorld(req, res) {
+    res.send('Hello World!')
+  }
+}
+
+/////////////////////////////////////////////////////////
+
+class KultHlinaPOSApplicationService {
+  constructor() {
+    this.posterProxy = new PosterProxyService()
+    this.kuktHliba = new KultHlibaPointOfSaleService()
+    this.webApp = new KultHlibaWebApp()
+    this.posterProxy.onConnectedToPoster(()=> this.kuktHliba.connectedToPoster())
+    this.posterProxy.onDisconnectedToPoster(()=> this.kuktHliba.disconnectedToPoster())
+  }
+
+  start() {
+    this.posterProxy.start()
+    this.kuktHliba.start()
+    this.webApp.start()
+  }
+}
+
+var app = new KultHlinaPOSApplicationService()
+app.start()
+
+
