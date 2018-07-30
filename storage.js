d808bab3a4cd74a3b78d345d854a1c4cf0163c6e
lyubomyr.semkiv <lyubomyr.semkiv@hotmail.com>
2018-07-30 22:09:31 +0300

    Added basic connection flow to storage server

--- storage.js
+++ storage.js
@@ -1,10 +1,18 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const STATE_CONNECTED = 'state-connected';
+const STATE_DISCONNECTED = 'state-disconnected';
+
 class StorageService {
   constructor(config) {
-    this.mongoPort = config.port
+    this.port = config.port;
+    this.state = STATE_DISCONNECTED;
     console.log('storage: mongo port: ' + this.mongoPort)
   }
 
   start() {
+    this._initiateConnect();
   }
 
   planItem() {
@@ -16,6 +24,23 @@ class StorageService {
   initialize() {
     console.log('storage: Initialized');
   }
+
+  _initiateConnect() {
+    console.log('_initiateConnect');
+    const url = 'mongodb://localhost:' + this.port + '/myproject';
+    const dbName = 'myproject';
+    console.log('storage: url: ' + url)
+    this.client = MongoClient.connect(url, (err, db) => {
+      if (!err && db) {
+        console.log('storage: connected to mongo: ' + JSON.stringify(db))
+        this.state = STATE_CONNECTED;
+      } else {
+        console.log('storage: error connection to mongo')
+        setTimeout(() => this._initiateConnect(), 1000);
+      }
+    });
+  }
 }
+
 
 module.exports = StorageService;
