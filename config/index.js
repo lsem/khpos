e8397ef308851572f5b7b62810452d51f0a726ea
lyubomyr.semkiv <lyubomyr.semkiv@hotmail.com>
2018-08-01 20:52:48 +0300

    Intoroduced loading secrets from env vars

--- config/index.js
+++ config/index.js
@@ -1,25 +1,44 @@
 // https://medium.com/@jinmatt/config-management-for-node-js-based-on-runtime-environment-variables-55b3c6d82f5c
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const NODE_ENV = process.env.NODE_ENV;
 let configBuffer = null;
 
 switch (NODE_ENV) {
-  case 'prod': {
-    configBuffer = fs.readFileSync(path.resolve(__dirname, 'prod.json'), 'utf-8');
+  case "prod": {
+    configBuffer = fs.readFileSync(path.resolve(__dirname, "prod.json"), "utf-8");
     break;
   }
-  case 'dev': {
-    configBuffer = fs.readFileSync(path.resolve(__dirname, 'dev.json'), 'utf-8');
+  case "dev": {
+    configBuffer = fs.readFileSync(path.resolve(__dirname, "dev.json"), "utf-8");
     break;
   }
   default: {
-    configBuffer = fs.readFileSync(path.resolve(__dirname, 'default.json'), 'utf-8');
+    configBuffer = fs.readFileSync(path.resolve(__dirname, "default.json"), "utf-8");
     break;
   }
 }
 
-let config = JSON.parse(configBuffer);
-module.exports = config;
+function expandEnvVars(config) {
+  for (var key in config) {
+    if (config.hasOwnProperty(key)) {
+      if (typeof config[key] === "string") {
+        if (config[key].startsWith("$")) {
+          const varName = config[key].slice(1);
+          if (process.env[varName]) {
+            config[key] = process.env[varName];
+          } else {
+            throw new Error(`expand: No var for variable: ${varName}`);
+          }
+        }
+      } else if (typeof (config[key] === "object")) {
+        expandEnvVars(config[key]);
+      }
+    }
+  }
+  return config;
+}
 
+let config = expandEnvVars(JSON.parse(configBuffer));
+module.exports = config;
