4a7786dbb181a7d55da38afb4208ea978ab3e390
lyubomyr.semkiv <lyubomyr.semkiv@hotmail.com>
2018-08-01 21:55:58 +0300

    Formatted app.js

--- khpos_ui/src/App.js
+++ khpos_ui/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class Products extends Component {
   constructor(props) {
@@ -11,31 +11,36 @@ class Products extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/products')
-    .then(json => json.data.response.map(result => (
-      {
-        id: result.product_id,
-        name: result.product_name,
-        category_name: result.category_name,
-        category_id: result.menu_category_id,
-        cost: result.cost
-      })))
-    .then(products => {
-      this.setState({products: products})
-    })
-    .catch(err => console.log('failed fetching products: ' + err));
+    axios
+      .get("http://localhost:5000/products")
+      .then(json =>
+        json.data.response.map(result => ({
+          id: result.product_id,
+          name: result.product_name,
+          category_name: result.category_name,
+          category_id: result.menu_category_id,
+          cost: result.cost
+        }))
+      )
+      .then(products => {
+        this.setState({ products: products });
+      })
+      .catch(err => console.log("failed fetching products: " + err));
   }
 
   render() {
-    const productItems = this.state.products.map((product) =>
-      <li key={product.id}> {product.name} ({product.category_name}) -- {product.cost}₴ </li>
-    );
+    const productItems = this.state.products.map(product => (
+      <li key={product.id}>
+        {" "}
+        {product.name} ({product.category_name}) -- {product.cost}₴{" "}
+      </li>
+    ));
     return (
       <div className="Products">
         <p> Продукти ({this.state.products.length} одиниць): </p>
         <ul> {productItems} </ul>
       </div>
-    )
+    );
   }
 }
 
@@ -47,7 +52,7 @@ class App extends Component {
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to KH POS</h1>
         </header>
-        <Products/>
+        <Products />
       </div>
     );
   }
