767f7e5a6c0d549a4161967f7b1ab29f5514a1d0
lyubomyr.semkiv <lyubomyr.semkiv@hotmail.com>
2018-08-05 19:51:21 +0300

    Removed unnecceary comments

--- back/KhPosWebApplication.js
+++ back/KhPosWebApplication.js
@@ -53,8 +53,8 @@ function errorHandler(err, req, res, next) {
     debug("appErrors.NotImplementedError")
     res.status(501).send(err.message);
   } else {
-    if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
-    res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
   }
 }
 
