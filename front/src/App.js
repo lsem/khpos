c49bee827128303ed8e039ad433b46867497122e
lyubomyr.semkiv <lyubomyr.semkiv@hotmail.com>
2018-09-02 01:50:37 +0300

    Minor refactoring

--- front/src/App.js
+++ front/src/App.js
@@ -268,78 +268,7 @@ class SchedulerTimeline extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      jobs: [
-        {
-          title: "1",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 2.5,
-          startTime: Date.parse("01 Jan 1970 00:00:00 GMT"),
-          id: "1"
-        },
-        {
-          title: "2",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 1.5,
-          startTime: Date.parse("01 Jan 1970 02:00:00 GMT"),
-          id: "2"
-        },
-        {
-          title: "3",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 3.5,
-          startTime: Date.parse("01 Jan 1970 01:00:00 GMT"),
-          id: "3"
-        },
-        {
-          title: "4",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 0.7,
-          startTime: Date.parse("01 Jan 1970 1:30:00 GMT"),
-          id: "4"
-        },
-        {
-          title: "5",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 0.7,
-          startTime: Date.parse("01 Jan 1970 6:30:00 GMT"),
-          id: "5"
-        },
-        {
-          title: "6",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 0.4,
-          startTime: Date.parse("01 Jan 1970 7:00:00 GMT"),
-          id: "6"
-        },
-        {
-          title: "7",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 1.2,
-          startTime: Date.parse("01 Jan 1970 7:35:00 GMT"),
-          id: "7"
-        },
-        {
-          title: "8",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 1.2,
-          startTime: Date.parse("01 Jan 1970 6:30:00 GMT"),
-          id: "8"
-        },
-        {
-          title: "9",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 1.2,
-          startTime: Date.parse("01 Jan 1970 7:00:00 GMT"),
-          id: "9"
-        },
-        {
-          title: "10",
-          tintColor: "rgb(216, 216, 216)",
-          durationHours: 1.2,
-          startTime: Date.parse("01 Jan 1970 7:30:00 GMT"),
-          id: "10"
-        }
-      ]
+      jobs: getSampleJobs()
     };
   }
 
