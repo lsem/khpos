6c8897aba5cdf8b8d5db5be62f45513e2a245da3
lyubomyr.semkiv <lyubomyr.semkiv@hotmail.com>
2018-09-27 20:31:55 +0300

    MINOR: Format AssignmentTimelne.jsx with built in vscode formatter

--- front/src/components/AssignmentsTimeline.jsx
+++ front/src/components/AssignmentsTimeline.jsx
@@ -1,70 +1,82 @@
-import React from 'react';
-import _ from 'lodash';
-import moment from 'moment';
-import './AssignmentsTimeline.css'
+import React from "react";
+import _ from "lodash";
+import moment from "moment";
+import "./AssignmentsTimeline.css";
 
 function AssignmentsTimeline(props) {
   const { jobs, columnWidth, msToPixels, beginTime, endTime } = props;
 
-  const groupByWorkers = (jobs) => {
-    let assignmentsByWorkers = {}
+  const groupByWorkers = jobs => {
+    let assignmentsByWorkers = {};
 
     _.forEach(jobs, j => {
       let startTime = j.startTime;
 
       _.forEach(j.techMap.tasks, t => {
-        let end = moment(startTime).add(t.durationMins, "minutes").valueOf()
-        
+        let end = moment(startTime)
+          .add(t.durationMins, "minutes")
+          .valueOf();
 
         if (t.assigned) {
           _.forEach(t.assigned, a => {
-            if (!assignmentsByWorkers[a.id])
-              assignmentsByWorkers[a.id] = []
-              assignmentsByWorkers[a.id].push({
+            if (!assignmentsByWorkers[a.id]) assignmentsByWorkers[a.id] = [];
+            assignmentsByWorkers[a.id].push({
               ...a,
               startTime,
               endTime: end,
               key: j.id + t.id + a.id
-            })
-          })
+            });
+          });
         }
         startTime += end - startTime;
-      })
-    })
-    return assignmentsByWorkers
-  }
+      });
+    });
+    return assignmentsByWorkers;
+  };
 
-  const renderAssignmentTimeSpan = (assignment) => {
+  const renderAssignmentTimeSpan = assignment => {
     const assignmentTimeSpanStyle = {
       backgroundColor: assignment.color,
       top: msToPixels(assignment.startTime - beginTime),
       height: msToPixels(assignment.endTime - assignment.startTime),
       width: columnWidth,
       borderRadius: columnWidth
-    }
+    };
 
-    return (<div style={assignmentTimeSpanStyle} className="assignmentsTimeSpan" key={assignment.key}></div>)
-  }
+    return (
+      <div
+        style={assignmentTimeSpanStyle}
+        className="assignmentsTimeSpan"
+        key={assignment.key}
+      />
+    );
+  };
 
-  const renderColumns = ( assignmentsByWorkers ) => {
+  const renderColumns = assignmentsByWorkers => {
     let left = 0;
     return _.map(assignmentsByWorkers, (abw, key) => {
       const columnStyle = {
         width: columnWidth,
-        height: msToPixels(endTime - beginTime),
+        height: msToPixels(endTime - beginTime)
         //left: left += columnWidth
-      }
+      };
       return (
-      <div className="assignmentsTimelineColumn" style={columnStyle} key={key}>
-        { _.map(abw, a => renderAssignmentTimeSpan(a)) }
-      </div>
-    )})}
+        <div
+          className="assignmentsTimelineColumn"
+          style={columnStyle}
+          key={key}
+        >
+          {_.map(abw, a => renderAssignmentTimeSpan(a))}
+        </div>
+      );
+    });
+  };
 
   return (
     <div className="assignmentsTimeline">
-      { renderColumns(groupByWorkers(jobs)) }
+      {renderColumns(groupByWorkers(jobs))}
     </div>
-  )
+  );
 }
 
-export default AssignmentsTimeline
+export default AssignmentsTimeline;
