fc03794a9fd79929c503afac14e6a199942d54c0
sarat12 <taras.if1@gmail.com>
2018-09-29 18:19:30 +0300

    Add redux-persist

--- front/src/index.js
+++ front/src/index.js
@@ -5,24 +5,48 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { offline } from "@redux-offline/redux-offline";
+import { createOffline } from "@redux-offline/redux-offline";
 import offlineConfig from "@redux-offline/redux-offline/lib/defaults";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import reducer from "./reducers/index";
 import axios from "axios";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const {
+  middleware: offlineMiddleware,
+  enhanceReducer: offlineEnhanceReducer,
+  enhanceStore: offlineEnhanceStore
+} = createOffline({
+  ...offlineConfig,
+  effect: (effect, _action) => axios(effect),
+  persist: false
+});
+
+const persistConfig = {
+  key: "persist",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, offlineEnhanceReducer(reducer));
 
 const store = createStore(
-  reducer,
+  persistedReducer,
   composeWithDevTools(
-    applyMiddleware(thunk),
-    offline({ ...offlineConfig, effect: (effect, _action) => axios(effect), persist: false })
+    applyMiddleware(thunk, offlineMiddleware),
+    offlineEnhanceStore
   )
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App/> 
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
