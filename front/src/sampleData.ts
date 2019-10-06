f09130df4dbe4531e69e6a3381617aec05b57da1
sarat12 <taras.if1@gmail.com>
2019-10-06 20:06:07 +0300

    Make techmaps queriable from API with data adoption

--- front/src/sampleData.ts
+++ front/src/sampleData.ts
@@ -11,16 +11,16 @@ import Job from "./models/plan/job";
 export function getTechMaps() : Array<TechMap> {
   return [
     {
-      id: "TMP-XXX-YYY-ZZZ-YY0",
+      id: "TM-5fee8d0d-1ec9-4dcd-91fe-312bfefde835",
       name: "Хліб Французький (КХ)",
       units: [1, 6, 8, 10, 12, 14, 16, 18, 20],
       steps: [
         {
-          id: "STP-XXX-YYY-ZZZ-YY0",
+          id: "STP-fb693c94-279b-441a-9731-9c823c97630e",
           name: "Замішування",
           ingredients: [
             { 
-              ingredientId: "ING-XXX-YYY-ZZZ-YY0",
+              ingredientId: "ING-e9c1bc32-1023-4930-8325-f9e8da28571b",
               countByUnits: new Map<number, number>([
                 [1, 292],
                 [6, 1752],
@@ -34,7 +34,7 @@ export function getTechMaps() : Array<TechMap> {
               ])
             } as IngredientsRow,
             { 
-              ingredientId: "ING-XXX-YYY-ZZZ-YY1",
+              ingredientId: "ING-e666a3b0-411e-4794-8d94-67e1ed4644d0",
               countByUnits: new Map<number, number>([
                 [1, 163],
                 [6, 978],
@@ -48,7 +48,7 @@ export function getTechMaps() : Array<TechMap> {
               ])
             } as IngredientsRow,
             { 
-              ingredientId: "ING-XXX-YYY-ZZZ-YY2",
+              ingredientId: "ING-5a7294f4-b311-4916-a4e7-7b78470a2201",
               countByUnits: new Map<number, number>([
                 [1, 78],
                 [6, 468],
@@ -94,7 +94,7 @@ export function getTechMaps() : Array<TechMap> {
           ],
           inventory: [
             {
-              deviceId: "INV-XXX-YYY-ZZZ,YY0",
+              deviceId: "INV-b5b5725d-bcf5-457e-a225-2c2941b8172f",
               countByUnits: new Map<number, number>([
                 [1, 1],
                 [6, 1],
@@ -111,13 +111,13 @@ export function getTechMaps() : Array<TechMap> {
           instructions: `{"blocks":[{"key":"2ic2d","text":"Відважити воду та пшеничну закваску в чисту і суху ємність відповідного об'єму.","type":"ordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"7pdcs","text":"Окремо відважити боршно в чисту і суху ємність відповідного об’єму.","type":"ordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"an4s6","text":"В спіральному тістомісі змішати компоненти з послідовності (1) на протязі 4—5 хвилин (в залежності від кількості замісу).","type":"ordered-list-item","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}`
         } as Step,
         {
-          id: "STP-XXX-YYY-ZZZ-YY1",
+          id: "STP-08e6a457-c60d-43b3-9f2a-b9a8c88e1e80",
           name: "Аутоліз",
           ingredients: [],
           humanResources: [],
           inventory: [
             {
-              deviceId: "INV-XXX-YYY-ZZZ,YY0",
+              deviceId: "INV-c5cbee18-f4d5-4283-b5fa-3990a501ae42",
               countByUnits: new Map<number, number>([
                 [1, 1],
                 [6, 1],
@@ -141,17 +141,17 @@ export function getTechMaps() : Array<TechMap> {
 export function getIngredients(): Array<Ingredient> {
   return [
     {
-      id: "ING-XXX-YYY-ZZZ-YY0",
+      id: "ING-e9c1bc32-1023-4930-8325-f9e8da28571b",
       name: "Борошно пшеничне 1 гат",
       units: "г"
     },
     {
-      id: "ING-XXX-YYY-ZZZ-YY1",
+      id: "ING-e666a3b0-411e-4794-8d94-67e1ed4644d0",
       name: "Вода",
       units: "мл"
     },
     {
-      id: "ING-XXX-YYY-ZZZ-YY2",
+      id: "ING-5a7294f4-b311-4916-a4e7-7b78470a2201",
       name: "Закваска пшенична",
       units: "г"
     }
@@ -161,7 +161,7 @@ export function getIngredients(): Array<Ingredient> {
 export function getInventory(): Array<Device> {
   return [
     {
-      id: "INV-XXX-YYY-ZZZ,YY0",
+      id: "INV-b5b5725d-bcf5-457e-a225-2c2941b8172f",
       name: "Спіральний тістоміс",
       units: "шт"
     }
--- front/src/store/techMaps/thunks.ts
+++ front/src/store/techMaps/thunks.ts
@@ -1,23 +1,57 @@
-import { getTechMaps } from "../../sampleData";
-import { Dispatch, ActionCreator } from "redux";
+import { ActionCreator } from "redux";
 import { techMapsRequestSucceeded, techMapsRequest } from "./actions";
 import { ThunkAction } from "redux-thunk";
 import { TechMapsActionTypes } from "./types";
 import { AppState } from "..";
+import TechMap from "../../models/techMaps/techMap";
+import { getApi } from "../../api";
+
+const techMapParserReviever: any = (key: string, value: any) => {
+  if (typeof value === "object" && value !== null) {
+    if (key === "countByUnits") {
+      const map = new Map();
+      Object.keys(value).forEach(key => {
+        map.set(+key, value[key]);
+      });
+      return map;
+    }
+  }
+  return value;
+};
+
+const techMapParserReplacer: any = (key: string, value: any) => {
+  if (key === "countByUnits") {
+    const o: { [k: string]: any } = {};
+
+    for (let e of value.entries()) o[e[0]] = e[1];
+
+    return o;
+  } else {
+    return value;
+  }
+};
 
 export const thunkRequestTechMaps: ActionCreator<
   ThunkAction<
-    Promise<TechMapsActionTypes>, // The type of function return
+    Promise<void>, // The type of function return
     AppState, // The type of global state
     null, // The type of the thunk parameter
     TechMapsActionTypes // The type of the last action to be dispatched
   >
 > = () => {
-  return async (dispatch: Dispatch) => {
+  return async dispatch => {
     dispatch(techMapsRequest());
-
-    const ingredients = await getTechMaps();
-
-    return dispatch(techMapsRequestSucceeded(ingredients));
+    fetch(`${getApi()}/techMaps`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+      }
+    })
+      .then(res => res.text())
+      .then(raw => {
+        const data = JSON.parse(raw, techMapParserReviever);
+        dispatch(techMapsRequestSucceeded(data as Array<TechMap>));
+      })
+      .catch(error => console.error(error));
   };
 };
