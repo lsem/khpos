d5683e5adfa1b9c1dbf27b3e956268f5b2a86a21
sarat12 <taras.if1@gmail.com>
2019-10-17 20:01:32 +0300

    Get back jobs end-to-end functional

--- front/src/store/plan/thunks.ts
+++ front/src/store/plan/thunks.ts
@@ -1,4 +1,3 @@
-import { getJobs } from "../../sampleData";
 import { Dispatch, ActionCreator } from "redux";
 import {
   requestJobs,
@@ -7,17 +6,22 @@ import {
   insertJob,
   deleteJob,
   patchJob,
-  assignJob
+  assignJob,
+  insertJobRollback,
+  deleteJobRollback,
+  patchJobRollback,
+  assignJobRollback
 } from "./actions";
 import TimeSpan from "../../models/plan/timeSpan";
 import Job from "../../models/plan/job";
 import { ThunkAction } from "redux-thunk";
 import { PlanActionTypes } from "./types";
 import { AppState } from "..";
+import { getApi } from "../../api";
 
 export const thunkSetTimeSpan: ActionCreator<
   ThunkAction<
-    Promise<PlanActionTypes>, // The type of function return
+    void, // The type of function return
     AppState, // The type of global state
     TimeSpan, // The type of the thunk parameter
     PlanActionTypes // The type of the last action to be dispatched
@@ -27,60 +31,107 @@ export const thunkSetTimeSpan: ActionCreator<
     dispatch(setTimeSpan(timeSpan));
     dispatch(requestJobs());
 
-    const jobs = await getJobs(timeSpan);
-
-    return dispatch(requestJobsSucceeded(jobs));
+    fetch(`${getApi()}/jobs`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        dispatch(requestJobsSucceeded(data));
+      })
+      .catch(error => console.error(error));
   };
 };
 
 export const thunkInsertJob: ActionCreator<
   ThunkAction<
-    Promise<PlanActionTypes>, // The type of function return
+    void, // The type of function return
     AppState, // The type of global state
     Job, // The type of the thunk parameter
     PlanActionTypes // The type of the last action to be dispatched
   >
 > = job => {
   return async (dispatch: Dispatch) => {
-    return dispatch(insertJob(job));
+    dispatch(insertJob(job)); //optimistic
+    fetch(`${getApi()}/jobs`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+      },
+      body: JSON.stringify(job)
+    }).catch(error => {
+      console.error(error);
+      dispatch(insertJobRollback(job));
+    });
   };
 };
 
 export const thunkDeleteJob: ActionCreator<
   ThunkAction<
-    Promise<PlanActionTypes>, // The type of function return
+    void, // The type of function return
     AppState, // The type of global state
     Job, // The type of the thunk parameter
     PlanActionTypes // The type of the last action to be dispatched
   >
 > = job => {
   return async (dispatch: Dispatch) => {
-    return dispatch(deleteJob(job));
+    dispatch(deleteJob(job)); //optimistic
+    fetch(`${getApi()}/jobs/${job.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+      }
+    }).catch(error => {
+      console.error(error);
+      dispatch(deleteJobRollback(job));
+    });
   };
 };
 
 export const thunkPatchJob: ActionCreator<
   ThunkAction<
-    Promise<PlanActionTypes>, // The type of function return
+    void, // The type of function return
     AppState, // The type of global state
     Job, // The type of the thunk parameter
     PlanActionTypes // The type of the last action to be dispatched
   >
 > = job => {
   return async (dispatch: Dispatch) => {
-    return dispatch(patchJob(job));
+    dispatch(patchJob(job)); //optimistic
+    fetch(`${getApi()}/jobs/${job.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+      },
+      body: JSON.stringify(job)
+    }).catch(error => {
+      console.error(error);
+      dispatch(patchJobRollback(job));
+    });
   };
 };
 
 export const thunkAssignJob: ActionCreator<
   ThunkAction<
-    Promise<PlanActionTypes>, // The type of function return
+    void, // The type of function return
     AppState, // The type of global state
     { jobId: string; stepId: string; employeeId: string }, // The type of the thunk parameter
     PlanActionTypes // The type of the last action to be dispatched
   >
 > = args => {
-  return async (dispatch: Dispatch) => {
-    return dispatch(assignJob(args.jobId, args.stepId, args.employeeId));
+  return async (dispatch: Dispatch, getState: () => AppState) => {
+    dispatch(assignJob(args.jobId, args.stepId, args.employeeId)); //optimistic
+    fetch(`${getApi()}/jobs/${args.jobId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+      },
+      body: JSON.stringify(getState().plan.jobs.find(j => j.id === args.jobId))
+    }).catch(error => {
+      console.error(error);
+      dispatch(assignJobRollback(args.jobId, args.stepId, args.employeeId));
+    });
   };
 };
