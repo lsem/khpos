ebdc844e8a54a47c902638b613ef137b84f7e5bb
sarat12 <taras.if1@gmail.com>
2019-10-19 15:27:25 +0300

    Get rid of axios in redux-offline due to poor flexibility

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -9,12 +9,11 @@ import { offline } from "@redux-offline/redux-offline";
 import offlineConfig from "@redux-offline/redux-offline/lib/defaults";
 import { Provider } from "react-redux";
 import { rootReducer } from "./store/index";
-import axios from "axios";
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk),
-    offline({ ...offlineConfig, effect: (effect, _action) => axios(effect), persist: (false as any)})
+    offline({ ...offlineConfig, persist: (false as any)})
   )
 );
 
