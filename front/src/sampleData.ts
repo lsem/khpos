16cda97644a8b2510de427931a0ac5ffe3dbe54a
sarat12 <taras.if1@gmail.com>
2019-10-19 15:28:26 +0300

    Add missing items in sampleData.ts

--- front/src/sampleData.ts
+++ front/src/sampleData.ts
@@ -114,8 +114,80 @@ export function getTechMaps() : Array<TechMap> {
         {
           id: "STP-08e6a457-c60d-43b3-9f2a-b9a8c88e1e80",
           name: "Аутоліз",
-          ingredients: [],
-          humanResources: [],
+          ingredients: [
+            { 
+              ingredientId: "ING-e9c1bc32-1023-4930-8325-f9e8da28571b",
+              countByUnits: new Map<number, number>([
+                [1, 292],
+                [6, 1752],
+                [8, 2336],
+                [10, 2920],
+                [12, 3504],
+                [14, 4088],
+                [16, 4672],
+                [18, 5256],
+                [20, 5840],
+              ])
+            } as IngredientsRow,
+            { 
+              ingredientId: "ING-e666a3b0-411e-4794-8d94-67e1ed4644d0",
+              countByUnits: new Map<number, number>([
+                [1, 163],
+                [6, 978],
+                [8, 1304],
+                [10, 1630],
+                [12, 1956],
+                [14, 2282],
+                [16, 2608],
+                [18, 2934],
+                [20, 3260],
+              ])
+            } as IngredientsRow,
+            { 
+              ingredientId: "ING-5a7294f4-b311-4916-a4e7-7b78470a2201",
+              countByUnits: new Map<number, number>([
+                [1, 78],
+                [6, 468],
+                [8, 624],
+                [10, 780],
+                [12, 936],
+                [14, 1092],
+                [16, 1248],
+                [18, 1404],
+                [20, 1560],
+              ])
+            } as IngredientsRow
+          ],
+          humanResources: [
+            {
+              peopleCount: 1,
+              countByUnits: new Map<number, number>([
+                [1, 30],
+                [6, 22],
+                [8, 25],
+                [10, 26],
+                [12, 28],
+                [14, 30],
+                [16, 32],
+                [18, 34],
+                [20, 36],                    
+              ])
+            } as HumanResourcesRow,
+            {
+              peopleCount: 2,
+              countByUnits: new Map<number, number>([
+                [1, 10],
+                [6, 15],
+                [8, 16],
+                [10, 17],
+                [12, 18],
+                [14, 19],
+                [16, 20],
+                [18, 21],
+                [20, 22],                    
+              ])
+            } as HumanResourcesRow
+          ],
           inventory: [
             {
               deviceId: "INV-c5cbee18-f4d5-4283-b5fa-3990a501ae42",
@@ -179,5 +251,25 @@ export function getJobs(timeSpan: TimeSpan): Array<Job> {
       productionQuantity: 1,
       employeesQuantity: 1
     } as Job
+  ]
+}
+
+export function getEmployees() {
+  return [
+    {
+      id: "EMP-33b9d3a4-4a74-4518-8fc0-4c895e39bab8",
+      firstName: "Аня",
+      color: "#5ac8fa"
+    },
+    {
+      id: "EMP-f3891a14-311b-4091-b7cd-134081eb099b",
+      firstName: "Вітя",
+      color: "#4cd964"
+    },
+    {
+      id: "EMP-08875fa6-3706-48df-b94c-4b8a4b5d05ed",
+      firstName: "Настя",
+      color: "#ffcc00"
+    }
   ]
 }
