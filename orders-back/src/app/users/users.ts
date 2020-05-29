580c6df6d0530bd093f8954fbb98bdf2354276fa
Liubomyr Semkiv <ext-liubomyr.semkiv@here.com>
2020-05-29 23:35:38 +0300

    Get users commands handlers respect permissions

--- orders-back/src/app/users/users.ts
+++ orders-back/src/app/users/users.ts
@@ -1,9 +1,17 @@
 import {AbstractStorage} from 'storage/AbstractStorage';
 import {EntityID} from 'types/core_types';
 import {UserModel} from 'types/domain_types';
+import {Issuer} from 'types/Issuer';
+import {PermissionFlags, UserPermissions} from 'types/Permissions';
 
-export async function createUser(storage: AbstractStorage, userIDName: string, userFullName: string,
+// todo: add email.
+export async function createUser(storage: AbstractStorage, issuer: Issuer, userIDName: string,
+                                 userPermissions: UserPermissions, userFullName: string,
                                  telNumber: string): Promise<EntityID> {
+  if (!(issuer.Permissions.mask & PermissionFlags.Admin)) {
+    throw new Error("Not allowed")
+  }
+
   const maybeExisting = await storage.findUserByIdName(userIDName);
   if (maybeExisting) {
     throw new Error("User with such IDName already exists")
@@ -18,10 +26,20 @@ export async function createUser(storage: AbstractStorage, userIDName: string, u
   return newUserID;
 }
 
-export async function getAllUsers(storage: AbstractStorage): Promise<ReadonlyArray<UserModel>> {
+export async function getAllUsers(storage: AbstractStorage,
+                                  issuer: Issuer): Promise<ReadonlyArray<UserModel>> {
+  if (!(issuer.Permissions.mask & PermissionFlags.Admin)) {
+    throw new Error("Not allowed")
+  }
   return storage.getAllUsers();
 }
 
-export async function getUser(storage: AbstractStorage, userID: EntityID): Promise<UserModel> {
+export async function getUser(storage: AbstractStorage, issuer: Issuer,
+                              userID: EntityID): Promise<UserModel> {
+
+  const isAdmin = (issuer.Permissions.mask & PermissionFlags.Admin) > 0;
+  if (!isAdmin && (issuer.ID.innerStr !== userID.innerStr)) {
+    throw new Error("Not allowed")
+  }
   return storage.getUser(userID);
 }
--- orders-back/src/app/users/users_test.ts
+++ orders-back/src/app/users/users_test.ts
@@ -1,27 +1,43 @@
-import {assert} from "chai";
+import {assert, expect} from "chai";
+import chai from 'chai';
+import chaiAsPromised from "chai-as-promised"
+import _ from 'lodash';
 import {InMemoryStorage} from "storage/InMemStorage";
+import {EntityID} from "types/core_types";
+import {Issuer} from "types/Issuer";
+import {PermissionFlags, UserPermissions} from "types/Permissions";
+
 import * as users from "./users";
+
+chai.use(chaiAsPromised);
+
+const AuthorizedIssuer =
+    new Issuer(new EntityID('USR'), new UserPermissions(PermissionFlags.Admin, []));
 
 describe("[users]", () => {
   it("should pass basic test", async () => {
     const storage = new InMemoryStorage();
-    const user1ID = await users.createUser(storage, "Ната", "Наталія Бушмак", "+380961112233");
-    assert.deepEqual(await users.getUser(storage, user1ID), {
+    const user1Permissions = new UserPermissions(PermissionFlags.Write | PermissionFlags.Write, []);
+    const user1ID = await users.createUser(storage, AuthorizedIssuer, "Ната", user1Permissions,
+                                           "Наталія Бушмак", "+380961112233");
+    assert.deepEqual(await users.getUser(storage, AuthorizedIssuer, user1ID), {
       userID : user1ID,
       userIdName : "Ната",
       userFullName : "Наталія Бушмак",
       telNumber : "+380961112233",
     });
 
-    assert.deepEqual(await users.getAllUsers(storage), [ {
+    assert.deepEqual(await users.getAllUsers(storage, AuthorizedIssuer), [ {
                        userID : user1ID,
                        userIdName : "Ната",
                        userFullName : "Наталія Бушмак",
                        telNumber : "+380961112233",
                      } ]);
 
-    const user2ID = await users.createUser(storage, "Вас", "Василь Тістоміс", "+380961112234");
-    assert.deepEqual(await users.getAllUsers(storage), [
+    const user2Permissions = _.clone(user1Permissions);
+    const user2ID = await users.createUser(storage, AuthorizedIssuer, "Вас", user2Permissions,
+                                           "Василь Тістоміс", "+380961112234");
+    assert.deepEqual(await users.getAllUsers(storage, AuthorizedIssuer), [
       {
         userID : user1ID,
         userIdName : "Ната",
@@ -39,18 +55,75 @@ describe("[users]", () => {
 
   it("should disallow creating duplicate users", async () => {
     const storage = new InMemoryStorage();
-    let firstCreated = false;
-    try {
-      const user1ID = await users.createUser(storage, "Ната", "Наталія Бушмак", "+380961112233");
-      firstCreated = true;
-      const user2ID = await users.createUser(storage, "Ната", "Наталія Бушмак", "+380961112233");
-      assert.fail("Should not create second user and go to catch");
-    } catch (err) {
-      assert.isTrue(firstCreated);
-      assert.instanceOf(err, Error);
-      assert.equal(err.message, "User with such IDName already exists");
-    }
+    const userPermissions = new UserPermissions(PermissionFlags.Write | PermissionFlags.Write, []);
+    const user1ID = await users.createUser(storage, AuthorizedIssuer, "Ната", userPermissions,
+                                           "Наталія Бушмак", "+380961112233");
+    await expect(users.createUser(storage, AuthorizedIssuer, "Ната", userPermissions,
+                                  "Наталія Бушмак", "+380961112233"))
+        .to.be.rejectedWith(Error, 'User with such IDName already exists');
+  });
+
+  it("should disallow creating users by non-authorized user", async () => {
+    const storage = new InMemoryStorage();
+    const userPermissions = new UserPermissions(PermissionFlags.Write | PermissionFlags.Write, []);
+
+    const NonAuthorizedIssuer = new Issuer(
+        new EntityID('USR'), new UserPermissions(PermissionFlags.Read | PermissionFlags.Write, []));
+
+    await expect(users.createUser(storage, NonAuthorizedIssuer, "Ната", userPermissions,
+                                  "Наталія Бушмак", "+380961112233"))
+        .to.be.rejectedWith(Error, 'Not allowed');
+
+    // Now try creating the same user with by Admin.
+    const AdminUser =
+        new Issuer(new EntityID('USR'), new UserPermissions(PermissionFlags.Admin, []));
+    const user1ID = await users.createUser(storage, AdminUser, "Ната", userPermissions,
+                                           "Наталія Бушмак", "+380961112233");
+  });
+
+  it("should disallow getting users by non-authorized user", async () => {
+    const storage = new InMemoryStorage();
+    const NonAuthorizedIssuer = new Issuer(
+        new EntityID('USR'), new UserPermissions(PermissionFlags.Read | PermissionFlags.Write, []));
+    await expect(users.getAllUsers(storage, NonAuthorizedIssuer))
+        .to.be.rejectedWith(Error, 'Not allowed')
+  });
+
+  it("should allow getting only self", async () => {
+    const storage = new InMemoryStorage();
+    const ivanPermissions = new UserPermissions(PermissionFlags.Write | PermissionFlags.Write, []);
+    const ivanID = await users.createUser(storage, AuthorizedIssuer, "ivan", ivanPermissions,
+                                          "Ivan Kruasan", "+33999333");
+    const ihorPermissions = new UserPermissions(PermissionFlags.Write | PermissionFlags.Write, []);
+    const ihorID = await users.createUser(storage, AuthorizedIssuer, "ihor", ihorPermissions,
+                                          "Ihor Sharlot", "+33999333");
+
+    const ivanAsIssuer = new Issuer(ivanID, ivanPermissions);
+
+    // Can read self.
+    const self = await users.getUser(storage, ivanAsIssuer, ivanID);
+    // todo: change deepEqual to deepSubset (and for other cases too)
+    assert.deepEqual(self, {
+      userID : ivanID,
+      userIdName : 'ivan',
+      userFullName : "Ivan Kruasan",
+      telNumber : "+33999333"
+    });
+
+    // Cannot read ihor
+    await expect(users.getUser(storage, ivanAsIssuer, ihorID))
+        .to.be.rejectedWith(Error, "Not allowed");
+
+    // Admin can read ihor.
+    const adminIssuer =
+        new Issuer(new EntityID('USR'), new UserPermissions(PermissionFlags.Admin, []));
+
+    const ihor = await users.getUser(storage, adminIssuer, ihorID);
+    assert.deepEqual(ihor, {
+      userID : ihorID,
+      userIdName : 'ihor',
+      userFullName : "Ihor Sharlot",
+      telNumber : "+33999333"
+    });
   });
 });
-
-// todo: test that inserting user with same ID or with same idname is error.
