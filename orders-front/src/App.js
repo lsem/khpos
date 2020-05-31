f121f2240b1d01c282f287013dd930134bbce8d1
sarat12 <taras.if1@gmail.com>
2020-05-31 11:17:50 +0300

    Enforce light theme

--- orders-front/src/App.js
+++ orders-front/src/App.js
@@ -1,36 +1,41 @@
 import React from "react";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
+//import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import ApplicationBar from "./features/ApplicationBar";
 import OrderManagement from "./features/orderManagement/OrderManagement";
 
 function App() {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  //const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? "dark" : "light",
-        },
-      }),
-    [prefersDarkMode]
-  );
+  // const theme = React.useMemo(
+  //   () =>
+  //     createMuiTheme({
+  //       palette: {
+  //         type: prefersDarkMode ? "dark" : "light",
+  //       },
+  //     }),
+  //   [prefersDarkMode]
+  // );
+
+  const theme = createMuiTheme({
+    palette: {
+      type: "light",
+    },
+  });
 
   return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div id="header">
-          <ApplicationBar />
-        </div>
-        <div id="main">
-          <OrderManagement />
-        </div>
-        <div id="footer">
-        </div>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div id="header">
+        <ApplicationBar />
+      </div>
+      <div id="main">
+        <OrderManagement />
+      </div>
+      <div id="footer"></div>
+    </ThemeProvider>
   );
 }
 
