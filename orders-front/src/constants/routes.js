4fd6c4be045be1ec721727697c188deac3cd744a
sarat12 <taras.if1@gmail.com>
2020-06-22 21:41:09 +0300

    Add logout route constant

--- orders-front/src/constants/routes.js
+++ orders-front/src/constants/routes.js
@@ -2,6 +2,7 @@ export default Object.freeze({
   orderManagement: "order-management",
   orderProduction: "order-production",
   login: "login",
+  logout: "logout",
 });
 
 export const orderManagementRoutes = Object.freeze({
